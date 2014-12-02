4d6ecf9a5e3770cb32edb9f9c2061c9e8457ad45
Marc Lewis <marc@innovotel.com>
2014-12-02 11:58:23 -0800

    Updates for latest version of gridview

--- widgets/GridView.php
+++ widgets/GridView.php
@@ -2,9 +2,7 @@
 namespace innovotel\widgets;
 
 /**
- * This displays a billing summary for the specified customer.
- * TODO:  If no customer is specified, it gets the customer ID from
- * the session.
+ * This displays a specialized grid view with Navigation on the right
  */
 
 use Yii;
@@ -21,53 +19,34 @@ class GridView extends \kartik\grid\GridView
 	public $condensed = true;
 	public $hover = true;
 	public $floatHeader = true;
-	public $export = false;
 
-
-	const CS_TEMPLATE = <<< HTML
-<div class="panel {type}">
-	<div class="panel-heading">
+	public $panelHeadingTemplate = <<< HTML
 	    <div class="panel-nav">{nav}</div>
 		<div class="panel-title"><h3 class="panel-title">{heading}</h3></div>
-	</div>
-	{before}
-	{items}
-	{after}
-	<div class="pull-right">{summary}</div>
-	<div class="clearfix"></div>
-	<div class="panel-footer">
+HTML;
+
+	public $panelFooterTemplate = <<< HTML
 		{footer}
 		<div class="kv-panel-pager pull-right">{pager}</div>
 		<div class="clearfix"></div>
-	</div>
-</div>
 HTML;
 
-    public function init()
-    {
-        parent::init();
-        GridViewAsset::register($this->getView());
-    }
+	public $toolbar = [];
 
-	protected function renderPanel()
+	public function init()
 	{
+		parent::init();
+
 		$nav = ArrayHelper::getValue($this->panel, 'nav', '');
 
-		$layout = strtr(self::CS_TEMPLATE, [
-			'{nav}' => $nav,
-		]);
+		$this->panelHeadingTemplate = strtr(
+			$this->panelHeadingTemplate,
+			[
+				'{nav}' => $nav,
+			]
+		);
 
-		$this->panel['layout'] = $layout;
-
-		/*
-		$showFooter = ArrayHelper::getValue($this->panel, 'showFooter', false);
-		$template = ($showFooter) ? self::TEMPLATE_1 : self::TEMPLATE_2;
-		$layout = ArrayHelper::getValue($this->panel, 'layout', $template);
-		$nav = ArrayHelper::getValue($layout, 'nav', '');
-		$this->panel = strtr(GridViewNav::CS_TEMPLATE, [
-			'{nav}' => 'wtf' . $nav,
-		]);*/
-		parent::renderPanel();
+		GridViewAsset::register($this->getView());
 	}
-
 }
+
